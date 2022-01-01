eb5cdda380848a334d0628df19a62f8e7cd89786
Hemanth Vijaywargi <hemanthvj12@gmail.com>
2022-01-01 15:16:01 +0530

    Added Express Server

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require("express");
+const { resolve } = require("path");
+
+// Server
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+// Statically Serving Build Folder
+const buildFolderPath = resolve(__dirname, "./build");
+app.use(express.static(buildFolderPath));
+
+// React Router
+app.get("*", (req, res) => {
+  res.sendFile(`${buildFolderPath}/index.html`);
+});
+
+// Starting the Server
+app.listen(PORT, () => {
+  console.log(`app listening on port : ${PORT}`);
+});
